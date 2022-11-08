301e9fb5f5485c5666b0923fc7cb4a57141ed7d0
OscarAguero92 <dr.aguero.cat@gmail.com>
2022-11-08 02:14:14 -0300

    commit

--- JavaScript/clima.js
+++ JavaScript/clima.js
@@ -43,3 +43,25 @@ window.addEventListener('load',()=>{
         })
     }
 })
+
+function getsource(id){
+    $.ajax({
+    url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=ddfef96feb36470cae0c55b3d9336155",
+    success: function(res) {
+    
+    document.getElementById("sourceLink").innerHTML=res.sourceUrl
+    document.getElementById("sourceLink").href=res.sourceUrl
+    }
+    });
+    }
+    function getrecepe(q){
+    $.ajax({
+    url:"https://api.spoonacular.com/recipes/search?apiKey=ddfef96feb36470cae0c55b3d9336155&lang=es&number=1&query="+q,
+    success: function(res) {
+    
+    document.getElementById("output").innerHTML="<h3>"+res.results[0].title+"</h3><img src='"+res.baseUri+res.results[0].image+"' width='200' /><br>Listo en tan solo "+res.results[0].readyInMinutes+" minutos"
+    getsource(res.results[0].id)
+    }
+    });
+    }
+    
