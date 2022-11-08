03d8be1e29628185f6898c61f1e710cae5b40fb6
OscarAguero92 <dr.aguero.cat@gmail.com>
2022-11-08 18:00:32 -0300

    cambios

--- Js/receta.js
+++ Js/receta.js
@@ -1,6 +1,6 @@
 function getsource(id){
     $.ajax({
-    url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=ddfef96feb36470cae0c55b3d9336155",
+    url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=87a331242f0248cbb04268ce661c8482",
     success: function(res) {
     
     document.getElementById("sourceLink").innerHTML=res.sourceUrl
@@ -10,7 +10,7 @@ function getsource(id){
     }
     function getrecepe(q){
     $.ajax({
-    url:"https://api.spoonacular.com/recipes/search?apiKey=ddfef96feb36470cae0c55b3d9336155&lang=es&number=1&query="+q,
+    url:"https://api.spoonacular.com/recipes/search?apiKey=87a331242f0248cbb04268ce661c8482&lang=es&number=1&query="+q,
     success: function(res) {
     
     document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><img src='"+res.baseUri+res.results[0].image+"' width='200' /><br>Listo en tan solo "+res.results[0].readyInMinutes+" minutos"
